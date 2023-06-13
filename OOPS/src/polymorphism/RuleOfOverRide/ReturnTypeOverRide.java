efdca28bf87b8257d26be5e6e69b09824325c340
Debasish Sahoo <debasish.sahoo96@gmail.com>
2023-06-13 16:25:57 +0530

    new update

--- OOPS/src/polymorphism/RuleOfOverRide/ReturnTypeOverRide.java
+++ OOPS/src/polymorphism/RuleOfOverRide/ReturnTypeOverRide.java
@@ -1,31 +1,33 @@
 package polymorphism.RuleOfOverRide;
 
 class SuperClass2 {
-	public Object method()
-	{
+	public Object method() {
 		System.out.println("This is the method in SuperClass");
 		return new Object();
 	}
 }
 
 class SubClass2 extends SuperClass2 {
-	public String method()
-	{
+	public String method() {
 		System.out.println("This is the method in SubClass");
 		return "Hello, World!";
 	}
 }
 
-
 public class ReturnTypeOverRide {
 
 	public static void main(String[] args) {
 		SuperClass2 obj1 = new SuperClass2();
-		obj1.method();
+		Object a = obj1.method();
+		System.out.println(a);
 
 		SubClass2 obj2 = new SubClass2();
-		obj2.method();
-
+		String b =obj2.method();
+		System.out.println(b);
+		
+		SuperClass2 obj3 = new SubClass2();
+		Object c = obj3.method();
+		System.out.println(c);
 	}
 
 }
